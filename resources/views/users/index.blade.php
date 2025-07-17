5f54c319985b7cc1988b7ed457c9df00fac3e591
distinctharsh <distinctharsh@gmail.com>
2025-07-17 14:29:21 +0530

    manager can change any user password

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -144,6 +144,29 @@ $breadcrumbs = [
                         <span class="text-info small">Default password will be assigned: <b>Welcome@123</b></span>
                     </div>
                     <div class="mb-3">
+                        <label for="password" class="form-label">Password</label>
+                        <div class="input-group">
+                            <input type="password" class="form-control @error('password') is-invalid @enderror" id="password" name="password" minlength="6">
+                            <button class="btn btn-outline-secondary" type="button" id="setDefaultPasswordBtn">Set Default Password</button>
+                            <button class="btn btn-outline-secondary toggle-password" type="button" data-target="#password" tabindex="-1">
+                                <i class="bi bi-eye-slash" id="togglePasswordIcon"></i>
+                            </button>
+                        </div>
+                        <small id="passwordHelpText" class="form-text text-muted">(leave blank to keep current)</small>
+                        @error('password')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label for="password_confirmation" class="form-label">Confirm Password</label>
+                        <div class="input-group">
+                            <input type="password" class="form-control" id="password_confirmation" name="password_confirmation">
+                            <button class="btn btn-outline-secondary toggle-password" type="button" data-target="#password_confirmation" tabindex="-1">
+                                <i class="bi bi-eye-slash" id="togglePasswordIconConfirm"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="mb-3">
                         <label for="role_id" class="form-label">Role</label>
                         <select class="form-select @error('role_id') is-invalid @enderror" id="role_id" name="role_id" required>
                             <option value="">Select a role</option>
@@ -322,6 +345,24 @@ $breadcrumbs = [
             $('#defaultPasswordNote').show();
             $('#formErrors').addClass('d-none');
         }
+
+        $('#setDefaultPasswordBtn').on('click', function() {
+            $('#password').val('Welcome@123');
+            $('#password_confirmation').val('Welcome@123');
+        });
+
+        // Toggle password visibility for password and confirmation fields
+        $(document).on('click', '.toggle-password', function() {
+            var target = $($(this).data('target'));
+            var icon = $(this).find('i');
+            if (target.attr('type') === 'password') {
+                target.attr('type', 'text');
+                icon.removeClass('bi-eye-slash').addClass('bi-eye');
+            } else {
+                target.attr('type', 'password');
+                icon.removeClass('bi-eye').addClass('bi-eye-slash');
+            }
+        });
     });
 </script>
 @endpush
