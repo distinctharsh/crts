912283b885e16bd637a7207d578a211c654481d8
distinctharsh <distinctharsh@gmail.com>
2026-07-14 12:38:29 +0530

    Fix Usage Report

--- resources/views/usage-report/index.blade.php
+++ resources/views/usage-report/index.blade.php
@@ -67,7 +67,7 @@
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                Total Tasks</div>
+                                Total Assigned Tasks</div>
                             <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $totalTasks }}</div>
                         </div>
                         <div class="col-auto">
@@ -84,7 +84,7 @@
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                Completed Tasks</div>
+                                Completed & Closed Tasks</div>
                             <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $totalCompleted }}</div>
                         </div>
                         <div class="col-auto">
