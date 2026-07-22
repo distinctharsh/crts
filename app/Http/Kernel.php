ef831caf4742b96904d78db225f58b80b08788a0
distinctharsh <distinctharsh@gmail.com>
2026-07-22 11:52:54 +0530

    Session problem fix

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -26,6 +26,11 @@ class Kernel extends HttpKernel
     protected $middlewareGroups = [
         'web' => [
             // Web middleware group
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\SessionTimeout::class,
         ],
 
